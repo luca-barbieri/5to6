0e59d5809d84f836cf82e1a6cedc185eac56f0de
Luca Barbieri <luca.barbieri@gmail.com>
2015-03-09 23:38:00 +0100

    5to6: add let/const test

---
+++ test/fixtures/transformation/5to6-let/misc/actual.js
@@ -0,0 +1,72 @@
+function a()
+{
+	if(a)
+		foo();
+	else
+		bar();
+
+	var v;
+	var z;
+	{
+		var a = 3;
+		var u = 6;
+		v = u;
+		var sa = 3, sb = 7;
+	}
+	{{{{
+		u += 3;
+		var f = function() {var v = 9;};
+		var b = a;
+		var c = b + b;
+		var b = 6;
+		var k = euoeuoeu + 6;
+		var x = function() {console.log(b);};
+		var y;
+		y = 88;
+		bab(z);
+	}}}}
+	x;
+	foo(miao);
+
+	var cc;
+	var clos = function() {console.log(cc);}
+	{
+		cc = 3;
+	}
+	{
+		cc = 4;
+	}
+
+	var nn, nc, ns;
+	{
+		nn = 4;
+		nc = 4;
+		ns = 4;
+		fizz(ns);
+	}
+	nn = 5;
+	nc = 5;
+	ns = 5;
+
+	var i;
+	for(i = 0; i < 10; ++i)	{
+		fooz(i);
+	}
+
+	var lou;
+        for(i = 0; i < 10; ++i) {
+		var il = i;
+                fooz(i);
+		xyz = function() {buz(il);};
+
+		for(var j = 0; j < 10; ++j) {}
+        }
+
+	var w;
+	for(k = 3, u = 7, w = 9; u;) {lou = 5;}
+	buzz(w);
+	lou;
+
+	function nested() {baz(nn); ns = 33;}
+	function nested2() {boz(ns);}
+}
